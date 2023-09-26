00f0ccd6e9bffe660a59e8ed670f316df3035f97
Shahil Patel <92860036+Shahil-Patel-3602@users.noreply.github.com>
2023-09-26 16:06:48 +0530

    Unshift method to add element at the beginning of the array.

---
+++ Array_methods/unshift.js
@@ -0,0 +1,10 @@
+// Add an element to the beginning of the array
+
+let arr = [1, 2, 3, 4, 5];
+
+console.log(arr);
+
+// Add an element to the beginning of the array
+arr.unshift(0);
+
+console.log(arr);
