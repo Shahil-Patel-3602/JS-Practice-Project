f40520087539c2c194848d19576fbdfe97585b6f
Shahil Patel <92860036+Shahil-Patel-3602@users.noreply.github.com>
2024-02-07 00:20:05 +0530

    Random Color

---
+++ Random Color/script.js
@@ -0,0 +1,17 @@
+let boxes = document.querySelectorAll('.box');
+
+const getRandomColor = () => {
+    // Formula = Math.ceil(min + (max-min)*(Math.random())
+    let val1 = Math.ceil(0 + (255 * Math.random()))
+    let val2 = Math.ceil(0 + (255 * Math.random()))
+    let val3 = Math.ceil(0 + (255 * Math.random()))
+    
+    return `rgb(${val1}, ${val2}, ${val3})`;
+}
+
+// Create array of boxes
+let boxesArray = Array.from(boxes);
+boxesArray.forEach(box => {
+    box.style.backgroundColor = getRandomColor();
+    box.style.color = getRandomColor();
+});
